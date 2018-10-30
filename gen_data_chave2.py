911f9dd2f619b192ff4a0673a052e4c025f6685d
Gustavo Fernandes dos Santos <gfdsantos@inf.ufpel.edu.br>
2018-10-30 14:29:37 -0300

    improvements

--- gen_data_chave2.py
+++ gen_data_chave2.py
@@ -1,8 +1,33 @@
+import string
+from itertools import permutations
+
 def totalOfCombinations():
-    comb = 8**(10 + 2*26)
+    comb = (10 + 2*26)**8
     return comb
 
 def numberChunksByChunkSize(chunkSize):
     comb = totalOfCombinations();
     numberChunks = comb / chunkSize
     return numberChunks
+
+def digits():
+  return string.digits
+
+def lowerletters():
+  return string.ascii_lowercase
+
+def upperletters():
+  return string.ascii_uppercase
+
+def genCombinations():
+  data = list(digits() + lowerletters() + upperletters())
+  combinations = list(permutations(data, 8))
+  return combinations
+
+def main():
+  combinations = genCombinations()
+  file = open('combinations.txt', 'w')
+  print(combinations)
+
+if __name__ == '__main__':
+  main()
--- test-sha.sh
+++ test-sha.sh
@@ -1,1 +1,11 @@
-echo $1 | openssl sha256 | openssl base64
+echo -n $1 | openssl dgst -md5 | sed 's/^.* //' | openssl base64
+
+# Hash original:             AKCkKOEHZuUPfCLbS0ye8WFnXoDEgw9RFYXlw293vbY=
+#                            MjVkNTVhZDI4M2FhNDAwYWY0NjRjNzZkNzEzYzA3YWQK
+#                            MWNhMzA4ZGY2Y2RiMGE4YmY0MGQ1OWJlMmExN2VhYzEgYmFzZTY0Cg==
+#
+# Tentativas
+# sha256 > base64 :          KHN0ZGluKT0gMjJkYmU2YjdiNzBhNjQ5NjZlMzE4MTNlNTk3ZGIzZTg2MzQ5MmQzNDFiZWUyZmIwNWMwZTg4NjQ3NzMzODdhZgo=
+# sha254 > rmd160 > base64:  KHN0ZGluKT0gNDkzYmZlNDY1MjQ5NWIwZmY5MTE5YzFlM2RmMDM2MWY2YjY5NjMxOAo=
+# sha1 > base64
+# md5 > base64               OGM4ODVmYWEzN2Y4MWE0YjI1ZGY1ZGU0Yzc2MGI0ZjkK (mesmo tamanho)
